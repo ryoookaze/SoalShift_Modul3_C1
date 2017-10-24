e0ea8aeaa14e22fe09e1c72eeff03839d405e780
rahadiankp <rahadian.k.putra@gmail.com>
2017-10-24 21:15:14 +0700

    fix file pointer, soal5 done

--- soal5.c
+++ soal5.c
@@ -6,7 +6,6 @@
 #define MAX_WORD 10
 
 struct find_word_t {
-    FILE *fi;
     char *word;
 };
 
@@ -22,15 +21,12 @@ int main(int argc, char *argv[]) {
     pthread_t tid[MAX_WORD];
     struct find_word_t cari[MAX_WORD];
 
-    FILE *fi = fopen("novel.txt", "r");
-
     for(i=1; i<argc; i++) {
-        cari[i-1].fi = fi;
         cari[i-1].word = argv[i];
     }
 
     for(i=0; i<argc-1; i++)
-        pthread_create(tid+i-1, NULL, &cariKata, (void *) &argv[i]);
+        pthread_create(tid+i, NULL, &cariKata, (void *) &cari[i]);
 
     for(i=0; i<argc-1; i++)
         pthread_join(tid[i], NULL);
@@ -38,5 +34,18 @@ int main(int argc, char *argv[]) {
 
 void* cariKata(void* argu) {
     struct find_word_t *fword = (struct find_word_t *) argu;
-    printf("%s\n", fword->word);
+    int count = 0;
+    char buffer[256];
+    FILE *he = fopen("novel.txt", "r");
+
+    while(fgets(buffer, 256, he) != NULL) {
+        char *buff = buffer;
+        while(buff = strstr(buff, fword->word)) {
+                count++;
+                buff++;
+        }
+    }
+
+    printf("%s : %d\n", fword->word, count);
+    fclose(he);
 }
