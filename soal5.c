fbfa649af5eec15d90280f9517bee4d5d71752ee
rahadiankp <rahadian.k.putra@gmail.com>
2017-10-24 20:13:12 +0700

    soal5 proto carKata

--- soal5.c
+++ soal5.c
@@ -8,7 +8,9 @@
 struct find_word_t {
     FILE *fi;
     char *word;
-}
+};
+
+void* cariKata(void* argu);
 
 int main(int argc, char *argv[]) {
     if(argc<2) {
@@ -18,10 +20,23 @@ int main(int argc, char *argv[]) {
 
     int i;
     pthread_t tid[MAX_WORD];
+    struct find_word_t cari[MAX_WORD];
 
-    for(i=1; i<argc; i++)
+    FILE *fi = fopen("novel.txt", "r");
+
+    for(i=1; i<argc; i++) {
+        cari[i-1].fi = fi;
+        cari[i-1].word = argv[i];
+    }
+
+    for(i=0; i<argc-1; i++)
         pthread_create(tid+i-1, NULL, &cariKata, (void *) &argv[i]);
 
     for(i=0; i<argc-1; i++)
         pthread_join(tid[i], NULL);
+}
+
+void* cariKata(void* argu) {
+    struct find_word_t *fword = (struct find_word_t *) argu;
+    printf("%s\n", fword->word);
 }
