477e2239f5d858a93ced6f3ee504ad7e6bac935a
rahadiankp <rahadian.k.putra@gmail.com>
2017-10-25 14:19:41 +0700

    soal1 penjual done

--- soal1_penjual.c
+++ soal1_penjual.c
@@ -21,7 +21,37 @@ int main() {
     stock = shmat(id1, NULL, 0);
     memset(stock, 0, sizeof(int)*6);
 
-    int sel;
+    int sel, st, i;
+    char na[10];
     while(1) {
+        printf("MENU PENJUAL BOSS!\n");
+        printf("1. Tambah stock\n");
+        printf("2. List barang\n");
+        printf("3. Keluar\n> ");
+        scanf("%d", &sel);
+
+        if(sel==1) {
+            scanf("%s", na);
+            scanf("%d", &st);
+            if(!strcmp(na, name[0])) stock[0]+=st;
+            if(!strcmp(na, name[1])) stock[1]+=st;
+            if(!strcmp(na, name[2])) stock[2]+=st;
+            if(!strcmp(na, name[3])) stock[3]+=st;
+            if(!strcmp(na, name[4])) stock[4]+=st;
+            if(!strcmp(na, name[5])) stock[5]+=st;
+        }
+        else if(sel==2) {
+            printf("\nSTOCK BARANG\n");
+            for(i=0; i<6; i++) {
+                if(stock[i]>0)
+                    printf("%s %d\n", name[i], stock[i]);
+            }
+            printf("\n");
+        }
+        else if(sel==3) break;
+        else {
+            printf("Salah\n");
+            continue;
+        }
     }
 }
